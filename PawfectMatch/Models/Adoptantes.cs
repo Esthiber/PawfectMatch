f134422589cc499ea6907230a1587acbe9a5a3b5
Esthiber <esthiber1@gmail.com>
2025-04-04 11:14:34 -0400

    Crear Clase Adoptantes

---
+++ PawfectMatch/Models/Adoptantes.cs
@@ -0,0 +1,21 @@
+﻿using PawfectMatch.Data;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PawfectMatch.Models
+{
+    public class Adoptantes
+    {
+        [Key]
+        public int AdoptanteId { get; set; }
+        [Required(ErrorMessage ="Nombre requerido")]
+        public string Nombre { get; set; } = null!;
+        [Required(ErrorMessage ="Ingrese su ocupacion")]
+        public string Ocupacion { get; set; } = null!;
+
+        public string UsuarioId { get; set; } = null!;
+
+        [ForeignKey("Id")]
+        public ApplicationUser User { get; set; } = null!;
+    }
+}
