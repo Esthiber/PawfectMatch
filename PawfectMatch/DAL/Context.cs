07c36cbfcdecb4e07e5c7fd01cd0ce1d19b7c22d
Esthiber <esthiber1@gmail.com>
2025-04-04 10:22:39 -0400

    Creacion del Contexto

---
+++ PawfectMatch/DAL/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PawfectMatch.DAL
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions options) : base(options)
+        {
+        }
+
+
+    }
+}
