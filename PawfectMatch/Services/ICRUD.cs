740d6301f5f94ebacf0884d858141bd1cacb36db
Esthiber <esthiber1@gmail.com>
2025-04-06 14:06:50 -0400

    add ICRUD service

---
+++ PawfectMatch/Services/ICRUD.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+
+namespace PawfectMatch.Services
+{
+    public interface ICRUD<T>
+    {
+        public Task<bool> InsertAsync(T elem);
+        public Task<bool> UpdateAsync(T elem);
+        public Task<bool> DeleteAsync(int id);
+        public Task<T> SearchByIdAsync(int id);
+        public Task<bool> ExistAsync(int id);
+        public Task<bool> SaveAsync(T elem);
+        public Task<List<T>> LiistAsync(Expression<Func<T, bool>> criteria);
+
+    }
+}
