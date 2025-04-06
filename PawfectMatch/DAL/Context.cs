05c8baa107dfdedd6ef9d9b754211a0f99c80a08
Esthiber <esthiber1@gmail.com>
2025-04-06 01:34:28 -0400

    Agregar modelos al contexto

--- PawfectMatch/DAL/Context.cs
+++ PawfectMatch/DAL/Context.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using PawfectMatch.Models;
+using PawfectMatch.Models._Mascotas;
+using PawfectMatch.Models._Solicitudes;
 
 namespace PawfectMatch.DAL
 {
@@ -8,6 +11,19 @@ namespace PawfectMatch.DAL
         {
         }
 
+
+        DbSet<Citas> Citas { get; set; }
+        DbSet<HistorialAdopciones> HistorialAdopciones { get; set; }
+
+        DbSet<SolicitudesAdopciones> SolicitudesAdopciones { get; set; }
+
+        DbSet<EstadoSolicitudes> EstadoSolicitudes { get; set; }
+        DbSet<Adoptantes> Adoptantes { get; set; }
+        DbSet<Mascotas> Mascotas { get; set; }
+        DbSet<Sexos> Sexos { get; set; }
+        DbSet<Razas> Razas { get; set; }
+        DbSet<Categorias> Categorias { get; set; }
+        DbSet<RelacionSizes> RelacionSizes { get; set; }
 
     }
 }
---
+++ PawfectMatch/Models/Citas.cs
@@ -0,0 +1,25 @@
+﻿using PawfectMatch.Models._Mascotas;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PawfectMatch.Models
+{
+    public class Citas
+    {
+        [Key]
+        public int CitaId { get; set; }
+
+        public int AdoptanteId { get; set; }
+
+        public int MascotaId { get; set; }
+
+        public DateTime FechaHora { get; set; }
+
+        [ForeignKey("AdoptanteId")]
+        public Adoptantes Adoptante { get; set; } = null!;
+
+        [ForeignKey("MascotaId")]
+        public Mascotas Mascota { get; set; } = null!;
+
+    }
+}
