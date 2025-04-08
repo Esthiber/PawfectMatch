34df131612ea13582e21761d40f03a4962ae07ac
Esthiber <esthiber1@gmail.com>
2025-04-08 18:37:22 -0400

    Migrations & Fixes

--- PawfectMatch/Data/Migrations/20250408212814_UpdateMascotas.Designer.cs
+++ PawfectMatch/Data/Migrations/20250408212814_UpdateMascotas.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PawfectMatch.Data;
 
@@ -11,9 +12,11 @@ using PawfectMatch.Data;
 namespace PawfectMatch.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250408212814_UpdateMascotas")]
+    partial class UpdateMascotas
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ PawfectMatch/Data/Migrations/20250408212814_UpdateMascotas.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PawfectMatch.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateMascotas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "SexoId",
+                table: "Mascotas",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Mascotas_SexoId",
+                table: "Mascotas",
+                column: "SexoId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Mascotas_Sexos_SexoId",
+                table: "Mascotas",
+                column: "SexoId",
+                principalTable: "Sexos",
+                principalColumn: "SexoId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Mascotas_Sexos_SexoId",
+                table: "Mascotas");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Mascotas_SexoId",
+                table: "Mascotas");
+
+            migrationBuilder.DropColumn(
+                name: "SexoId",
+                table: "Mascotas");
+        }
+    }
+}
